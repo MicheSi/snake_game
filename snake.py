127d30a65a94a9d063f53c7b0962245f160340f9
mlazzell17@gmail.com <MicheSi>
2020-10-06 13:38:29 -0700

    added method to move snake

--- snake.py
+++ snake.py
@@ -75,4 +75,20 @@ class Board(Canvas):
                 x, y = self.coords(apple)
                 self.create_image(x, y, image=self.dot, anchor=NW, tage="dot")
                 self.locateApple()
-                
+
+    # moves snake object
+    def moveSnake(self):
+        dots = self.find_withtag("dot")
+        head = self.find_withtag("head")
+
+        items = dots + head
+
+        spot = 0
+
+        while spot < len(items) - 1:
+            coordinate1 = self.coordinates(items[spot])
+            coordinate2 = self.coordinates(items[spot + 1])
+            self.move(items[spot], coordinate2[0] - coordinate1[0], coordinate2[1] - coordinate1[1])
+            spot += 1
+
+        self.move(head, self.moveX, self.moveY)    

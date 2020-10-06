25d1851fb36602ce72b24b1a4194d2b70d97b5cf
mlazzell17@gmail.com <MicheSi>
2020-10-06 12:49:28 -0700

    created class to initialize game

--- snake.py
+++ snake.py
@@ -3,9 +3,38 @@ import random
 from PIL import Image, ImageTk
 from tkinter import Tk, Frame, Canvas, ALL, NW
 
+# constants used in game
 class Constants:
     BOARD_WIDTH = 300
     BOARD_HEIGHT = 300
     DELAY = 100
     DOT_SIZE = 10
     MAX_RAND_POS = 27
+
+class Board(Canvas):
+    def __init__(self):
+        super().__init__(width=Constants.BOARD_WIDTH, height=Constants.BOARD_HEIGHT, background='black', highlightthickness=0)
+
+        self.initGame()
+        self.pack()
+
+    # initializes game
+    def initGame(self):
+        self.inGame = True
+        self.dots = 3
+        self.score = 0
+
+        # variables used to move snake object
+        self.moveX = Constants.DOT_SIZE
+        self.moveY = 0
+
+        # starting apple coordinates
+        self.appleX = 100
+        self.appleY = 190
+
+        self.loadImages()
+
+        self.createObjects()
+        self.locateApple()
+        self.bind_all("<Key>", self.onKeyPressed)
+        self.after(Constants.DELAY, self.onTimer)
